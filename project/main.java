18060041b88c2c255ebc03941e38c529cd8d2adb
nwms-tutoring <96587851+nwms-tutoring@users.noreply.github.com>
2025-08-04 15:24:48 -0500

    Update main.java

--- project/main.java
+++ project/main.java
@@ -5,24 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = project.RandomNumberGenerator.random(100, 999);
-        System.out.println("I've picked a random 3 digit number. Try to guess it!");
-        boolean bool = true;
+        System.out.println("I've picked a random 3 digit number. Try to guess it! You get 25 guesses.");
+        int guesses = 25;
 
-
-        while (true) {
+        while (guesses <= 25) {
             System.out.print("Your guess: ");
             int guess = scan.nextInt();
+            guesses--;  // Decrement guesses each time
 
             if (guess == num) {
-                System.out.println("You got it!");
+                System.out.println("You got it! 🐂🐂🐂");
                 break;
             } else if (guess < num) {
-                System.out.println("Too low!");
+                System.out.println("Too low! " + guesses + " guesses left!");
             } else {
-                System.out.println("Too high!");
+                System.out.println("Too high! " + guesses + " guesses left!");
             }
         }
-        
+
+        if (guesses == 0) {
+            System.out.println("Out of guesses! The number was " + num + ".");
+        }
     }
 }
 
