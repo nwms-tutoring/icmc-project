7135b3ab1fe30c678fb45df595bc884fee08fa4e
nwms-tutoring <96587851+nwms-tutoring@users.noreply.github.com>
2025-08-04 15:08:57 -0500

    change main.java

--- project/main.java
+++ project/main.java
@@ -1,2 +1,28 @@
-package project
-// different comment
+package project;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = project.RandomNumberGenerator.random(100, 999);
+        System.out.println("I've picked a random 3 digit number. Try to guess it!");
+        boolean bool = true;
+
+
+        while (true) {
+            System.out.print("Your guess: ");
+            int guess = scan.nextInt();
+
+            if (guess == num) {
+                System.out.println("You got it!");
+                break;
+            } else if (guess < num) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Too high!");
+            }
+        }
+        
+    }
+}
+
